0b53d555a383502fba911b3dcd0e87fa16361c70
Nanderson Matheus Reis <87582433+nandmat@users.noreply.github.com>
2022-05-11 21:56:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function GerarQRCode() {
+    var inputUsuario = document.querySelector('textarea').value;
+
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=H&chl=';
+
+    var conteudoQRCode = GoogleChartAPI + encodeURIComponent(inputUsuario);
+
+
+
+    document.querySelector('#qrcodeImage').src = conteudoQRCode;
+}
